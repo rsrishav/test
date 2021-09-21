44d5d186ccdc5fffc9a71727c073ad225305e326
Rishav Sharma <rsrishav0@gmail.com>
2021-09-21 08:33:27 +0530

    t

--- ims/settings.py
+++ ims/settings.py
@@ -130,7 +130,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / "main/static",
+    BASE_DIR / "register/static",
+    BASE_DIR / "products/static",
+    BASE_DIR / "masters/static",
+    BASE_DIR / "ims/staticfiles"
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
@@ -145,4 +151,3 @@ LOGOUT_REDIRECT_URL = "/login"
 CONSTANTS = constants
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
